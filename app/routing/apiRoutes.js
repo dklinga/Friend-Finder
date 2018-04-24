83461ff471e00920898ce4b6cf06aef63cae4355
dklinga <34742535+dklinga@users.noreply.github.com>
2018-04-24 10:01:57 -0400

    Update apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,47 @@
-var a = process.argv[2];
-var b = process.argv[3];
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendData = require("..app/data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+   
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+  
+    // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newFriends =req.body;
+   // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newFriend.routeName = newFriends.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newFriend);
+
+  characters.push(newFriend);
+
+  res.json(newFriend);
+}); 
+    
+    
