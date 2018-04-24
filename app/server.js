4b8d4aad07daf452a947f40decaea01201290434
dklinga <34742535+dklinga@users.noreply.github.com>
2018-04-24 08:46:17 -0400

    Delete server.js

--- app/server.js
+++
@@ -1,8 +0,0 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
-
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
