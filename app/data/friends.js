24fe8800a815219bc8ec9c15f8756df53715269b
dklinga <34742535+dklinga@users.noreply.github.com>
2018-04-24 08:48:21 -0400

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,16 @@
-var a = process.argv[2];
-var b = process.argv[3];
-
-if (a === b) {
-  console.log(true);
-}
-else {
-  console.log(false);
+{
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
 }
