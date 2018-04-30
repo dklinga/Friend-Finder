9c9e0e82824fb3462f1b8c8e3e35309b8568591a
Dan Klinga <dklinga@Dans-MBP.fios-router.home>
2018-04-30 13:06:10 -0400

    monday1

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,12 +5,12 @@ var friendData = [
   "photo":"https://vignette.wikia.nocookie.net/marvelcentral/images/4/4a/Tony-Stark-iron-man-11234572-1485-2061.jpg/revision/latest?cb=20110219055106",
   "scores":[
       5,
-      1,
-      4,
-      4,
-      5,
-      1,
       2,
+      4,
+      2,
+      4,
+      1,
+      1,
       5,
       4,
       1
